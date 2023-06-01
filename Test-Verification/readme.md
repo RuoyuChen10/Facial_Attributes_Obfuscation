7461ae3ad62712f907b771a7ea4d441e647b5a4f
RuoyuChen10 <2389388826@qq.com>
2023-06-02 03:44:46 +0800

    add readme

---
+++ Test-Verification/readme.md
@@ -0,0 +1,14 @@
+## Download pretrained model for face verification:
+
+Download Link: [https://pan.baidu.com/s/1_0xzxwpVVf3TnLhDyvfE9Q](https://pan.baidu.com/s/1_0xzxwpVVf3TnLhDyvfE9Q)
+
+Code: mm21
+
+put the download file to `Test-Verification/pre-trained`.
+
+## Results used for comparison
+
+Download Link: [https://pan.baidu.com/s/16zJWhSbtEeXZxgp13EFiaA](https://pan.baidu.com/s/16zJWhSbtEeXZxgp13EFiaA)
+
+Code: mm21
+put the download file to `Test-Verification/results`
--- Test-Verification/run.sh
+++ Test-Verification/run.sh
@@ -1,15 +1,15 @@
-CUDA_VISIBLE_DEVICES=1 python Verification.py --Net-type ArcFace-r100 --real-dir /exdata/HLt/StarGAN_all_file/StarGAN_base/stargan_celeba_new_att/results_test1/real_ima --fake-dir /exdata/HLt/StarGAN_all_file/StarGAN_base/stargan_celeba_new_att/results_test1/fake_ima
-CUDA_VISIBLE_DEVICES=1 python Verification.py --Net-type CosFace-r100 --real-dir /exdata/HLt/StarGAN_all_file/StarGAN_base/stargan_celeba_new_att/results_test1/real_ima --fake-dir /exdata/HLt/StarGAN_all_file/StarGAN_base/stargan_celeba_new_att/results_test1/fake_ima
-CUDA_VISIBLE_DEVICES=1 python Verification.py --Net-type VGGFace2 --real-dir /exdata/HLt/StarGAN_all_file/StarGAN_base/stargan_celeba_new_att/results_test1/real_ima --fake-dir /exdata/HLt/StarGAN_all_file/StarGAN_base/stargan_celeba_new_att/results_test1/fake_ima
+CUDA_VISIBLE_DEVICES=1 python Verification.py --Net-type ArcFace-r100 --real-dir ./results/results_test1/real_ima --fake-dir ./results/results_test1/fake_ima
+CUDA_VISIBLE_DEVICES=1 python Verification.py --Net-type CosFace-r100 --real-dir ./results/results_test1/real_ima --fake-dir ./results/results_test1/fake_ima
+CUDA_VISIBLE_DEVICES=1 python Verification.py --Net-type VGGFace2 --real-dir ./results/results_test1/real_ima --fake-dir ./results/results_test1/fake_ima
 
-CUDA_VISIBLE_DEVICES=1 python Verification.py --Net-type ArcFace-r100 --real-dir /exdata/HLt/StarGAN_all_file/StarGAN_base/stargan_celeba_new_att/results_test2/real_ima --fake-dir /exdata/HLt/StarGAN_all_file/StarGAN_base/stargan_celeba_new_att/results_test2/fake_ima
-CUDA_VISIBLE_DEVICES=1 python Verification.py --Net-type CosFace-r100 --real-dir /exdata/HLt/StarGAN_all_file/StarGAN_base/stargan_celeba_new_att/results_test2/real_ima --fake-dir /exdata/HLt/StarGAN_all_file/StarGAN_base/stargan_celeba_new_att/results_test2/fake_ima
-CUDA_VISIBLE_DEVICES=1 python Verification.py --Net-type VGGFace2 --real-dir /exdata/HLt/StarGAN_all_file/StarGAN_base/stargan_celeba_new_att/results_test2/real_ima --fake-dir /exdata/HLt/StarGAN_all_file/StarGAN_base/stargan_celeba_new_att/results_test2/fake_ima
+CUDA_VISIBLE_DEVICES=1 python Verification.py --Net-type ArcFace-r100 --real-dir ./results/results_test2/real_ima --fake-dir ./results/results_test2/fake_ima
+CUDA_VISIBLE_DEVICES=1 python Verification.py --Net-type CosFace-r100 --real-dir ./results/results_test2/real_ima --fake-dir ./results/results_test2/fake_ima
+CUDA_VISIBLE_DEVICES=1 python Verification.py --Net-type VGGFace2 --real-dir ./results/results_test2/real_ima --fake-dir ./results/results_test2/fake_ima
 
-CUDA_VISIBLE_DEVICES=1 python Verification.py --Net-type ArcFace-r100 --real-dir /exdata/HLt/StarGAN_all_file/StarGAN_base/stargan_celeba_new_att/results_test3/real_ima --fake-dir /exdata/HLt/StarGAN_all_file/StarGAN_base/stargan_celeba_new_att/results_test3/fake_ima
-CUDA_VISIBLE_DEVICES=1 python Verification.py --Net-type CosFace-r100 --real-dir /exdata/HLt/StarGAN_all_file/StarGAN_base/stargan_celeba_new_att/results_test3/real_ima --fake-dir /exdata/HLt/StarGAN_all_file/StarGAN_base/stargan_celeba_new_att/results_test3/fake_ima
-CUDA_VISIBLE_DEVICES=1 python Verification.py --Net-type VGGFace2 --real-dir /exdata/HLt/StarGAN_all_file/StarGAN_base/stargan_celeba_new_att/results_test3/real_ima --fake-dir /exdata/HLt/StarGAN_all_file/StarGAN_base/stargan_celeba_new_att/results_test3/fake_ima
+CUDA_VISIBLE_DEVICES=1 python Verification.py --Net-type ArcFace-r100 --real-dir ./results/results_test3/real_ima --fake-dir ./results/results_test3/fake_ima
+CUDA_VISIBLE_DEVICES=1 python Verification.py --Net-type CosFace-r100 --real-dir ./results/results_test3/real_ima --fake-dir ./results/results_test3/fake_ima
+CUDA_VISIBLE_DEVICES=1 python Verification.py --Net-type VGGFace2 --real-dir ./results/results_test3/real_ima --fake-dir ./results/results_test3/fake_ima
 
-CUDA_VISIBLE_DEVICES=1 python Verification.py --Net-type ArcFace-r100 --real-dir /exdata/HLt/StarGAN_all_file/StarGAN_base/stargan_celeba_new_att/results_test4/real_ima --fake-dir /exdata/HLt/StarGAN_all_file/StarGAN_base/stargan_celeba_new_att/results_test4/fake_ima
-CUDA_VISIBLE_DEVICES=1 python Verification.py --Net-type CosFace-r100 --real-dir /exdata/HLt/StarGAN_all_file/StarGAN_base/stargan_celeba_new_att/results_test4/real_ima --fake-dir /exdata/HLt/StarGAN_all_file/StarGAN_base/stargan_celeba_new_att/results_test4/fake_ima
-CUDA_VISIBLE_DEVICES=1 python Verification.py --Net-type VGGFace2 --real-dir /exdata/HLt/StarGAN_all_file/StarGAN_base/stargan_celeba_new_att/results_test4/real_ima --fake-dir /exdata/HLt/StarGAN_all_file/StarGAN_base/stargan_celeba_new_att/results_test4/fake_ima
+CUDA_VISIBLE_DEVICES=1 python Verification.py --Net-type ArcFace-r100 --real-dir ./results/results_test4/real_ima --fake-dir ./results/results_test4/fake_ima
+CUDA_VISIBLE_DEVICES=1 python Verification.py --Net-type CosFace-r100 --real-dir ./results/results_test4/real_ima --fake-dir ./results/results_test4/fake_ima
+CUDA_VISIBLE_DEVICES=1 python Verification.py --Net-type VGGFace2 --real-dir ./results/results_test4/real_ima --fake-dir ./results/results_test4/fake_ima
